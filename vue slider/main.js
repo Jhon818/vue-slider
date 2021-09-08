daf673c87c695cac37a1ec8bfe3dfa7faad6eee7
giov <pintogiovanni818@gmail.com>
2021-09-08 15:03:21 +0200

    parte grafica iniziale

---
+++ vue slider/main.js
@@ -0,0 +1,16 @@
+Vue.config.devtools = true;
+
+const app = new Vue( {
+    el: "#root",
+        data: {
+            photos: ["../vue slider/img/image1.jpg",
+            "../vue slider/img/image2.jpg",
+            "../vue slider/img/image3.jpg",
+            "../vue slider/img/image4.jpg"
+        ]
+            
+    },
+   
+  
+ 
+});
